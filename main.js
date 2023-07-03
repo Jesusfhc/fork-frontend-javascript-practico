9ba6d5ca6a45b51f7cad6b15df882ed0a115b5f5
Jesus Hernandez <jjesusfhernandezc@gmail.com>
2023-07-02 20:12:52 -0400

    anexado clase 13 al index y configurado para que solo se muestre un menu a la vez

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ let email = document.querySelector(".navbar-email");
 let desktopMenu = document.querySelector(".desktop-menu");
 
 email.addEventListener("click", () => {
+    isDesktopMenuClose = desktopMenu.classList.contains('inactive');
+    isCarShopClose = carASide.classList.contains('inactive');
+
+    if (isCarShopClose === false) {
+        carASide.classList.toggle('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 })
 
@@ -9,5 +16,27 @@ let hamburguerMenu = document.querySelector(".menu");
 let mobileMenu = document.querySelector(".mobile-menu");
 
 hamburguerMenu.addEventListener("click", () => {
+    isCarShopClose = carASide.classList.contains('inactive');
+
+    if (isCarShopClose === false) {
+        carASide.classList.toggle('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+})
+
+let shoppingCar = document.querySelector(".navbar-shopping-cart");
+let carASide = document.querySelector(".product-detail");
+
+shoppingCar.addEventListener("click", () => {
+    isDesktopMenuClose = desktopMenu.classList.contains('inactive');
+    isMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+    if (isDesktopMenuClose === false) {
+        desktopMenu.classList.toggle('inactive');
+    } else if (isMobileMenuClose === false) {
+        mobileMenu.classList.toggle('inactive');
+    }
+
+    carASide.classList.toggle('inactive');
 })
